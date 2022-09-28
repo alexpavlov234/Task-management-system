bc353d237a295e70a3698158acb3518f86297c61
Alexander Pavlov <alexpavlov234@outlook.com>
2022-09-28 15:05:02 +0300

    Adding Syncfusion to the project.

--- Task management system/Program.cs
+++ Task management system/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Task_management_system.Data;
-
+using Syncfusion.Blazor;
 namespace Task_management_system
 {
     public class Program
@@ -14,12 +14,14 @@ namespace Task_management_system
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
-
+            builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
             var app = builder.Build();
 
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzI0NjE1QDMyMzAyZTMyMmUzMGorYUM4M3ljdDEvMkRNMUxBSVJ0bGRQc01uZ2RHbGVnamM0QWZ4MjJmLzg9");
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
+                
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
